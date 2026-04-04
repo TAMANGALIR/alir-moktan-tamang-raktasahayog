8b011383ae5adeac8d8eed3f2f79ed72be3c1d79
TAMANGALIR <tamangalir@gmail.com>
2026-04-04 20:55:04 +0545

    fix: vercel prod fix

--- server/api/index.ts
+++ server/api/index.ts
@@ -31,7 +31,7 @@ app.use(cors({
     // Add your local and production URLs here
     origin: [
         "http://localhost:5173", 
-        "https://alir-moktan-tamang-raktasahayog.vercel.app/"
+        "https://alir-moktan-tamang-raktasahayog.vercel.app"
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
